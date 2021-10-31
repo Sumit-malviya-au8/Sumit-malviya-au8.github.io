6ddf5be89eabe4323267976dfe7517f4999421cc
Sumit-malviya-au8 <sumitindian11@gmail.com>
2021-10-31 16:28:44 +0530

    Partial task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from "react";
+
 
 function App() {
+  let today = new Date();
+  const [currentdate, setcurrentdate] = useState(today);
+
+  let date = `${today.getDate()}/${
+    today.getMonth() + 1
+  }/${today.getFullYear()}`;
+
+  function prevWeek() {
+    var prevWeek = new Date(
+      currentdate.getFullYear(),
+      currentdate.getMonth(),
+      currentdate.getDate() - 7
+    );
+    setcurrentdate(prevWeek);
+    console.log(currentdate);
+    return prevWeek;
+  }
+
+  function nextweek() {
+    var nextweek = new Date(
+      currentdate.getFullYear(),
+      currentdate.getMonth(),
+      currentdate.getDate() + 7
+    );
+    setcurrentdate(nextweek);
+    console.log(currentdate);
+    return nextweek;
+  }
+
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var d = new Date(currentdate);
+  var dayName = days[d.getDay()];
+  var dayName1 = days[d.getDay() + 1];
+  var dayName2 = days[d.getDay() + 2];
+  var dayName3 = days[d.getDay() + 3];
+  var dayName4 = days[d.getDay() + 4];
+  var dayName5 = days[d.getDay() + 5];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={prevWeek}>Prev week</button>
+      <p>{date}</p>
+      {console.log(date)}
+      <button onClick={nextweek}>Next week</button>
+
+      <p>{dayName}</p>
+      <p>{dayName1}</p>
+      <p>{dayName2}</p>
+      <p>{dayName3}</p>
+      <p>{dayName4}</p>
+
     </div>
   );
 }
